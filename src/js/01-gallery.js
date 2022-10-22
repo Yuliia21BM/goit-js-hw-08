67cfe5db5db5d98448a2d89b9e7c74c59ecbdbd8
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-10-22 17:57:28 +0300

    Add settings and 1 ex

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const gallerySet = createGalleryset(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', gallerySet);
+
+galleryRef.addEventListener('click', onAddItemsClick);
+
+function createGalleryset(items =[]) {
+    return items.map((item) => 
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+}
+
+function onAddItemsClick(evt) {
+    evt.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
